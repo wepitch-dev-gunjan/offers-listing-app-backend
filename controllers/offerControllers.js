95bd8fbda3613974260f89b3c56f5d460fa7d877
gunjan <gunjan.soral@gmail.com>
2024-04-12 15:47:12 +0530

    asdlkfjweoiru

--- controllers/offerControllers.js
+++ controllers/offerControllers.js
@@ -44,7 +44,7 @@ exports.getOffers = async (req, res) => {
 // Controller to get a specific offer by ID
 exports.getOffer = async (req, res) => {
   try {
-    const offer = await Offer.findById(req.params.id).populate('category').populate('brand');
+    const offer = await Offer.findById(req.params.offer_id).populate('category').populate('brand');
     if (!offer) {
       return res.status(404).json({ error: 'Offer not found' });
     }
@@ -59,7 +59,7 @@ exports.getOffer = async (req, res) => {
 exports.putOffer = async (req, res) => {
   try {
     const { name, category, brand, location, description, image, expire_at, discount_value } = req.body;
-    const updatedOffer = await Offer.findByIdAndUpdate(req.params.id, { name, category, brand, location, description, image, expire_at, discount_value }, { new: true });
+    const updatedOffer = await Offer.findByIdAndUpdate(req.params.offer_id, { name, category, brand, location, description, image, expire_at, discount_value }, { new: true });
     if (!updatedOffer) {
       return res.status(404).json({ error: 'Offer not found' });
     }
@@ -73,7 +73,7 @@ exports.putOffer = async (req, res) => {
 // Controller to delete a specific offer by ID
 exports.deleteOffer = async (req, res) => {
   try {
-    const deletedOffer = await Offer.findByIdAndDelete(req.params.id);
+    const deletedOffer = await Offer.findByIdAndDelete(req.params.offer_id);
     if (!deletedOffer) {
       return res.status(404).json({ error: 'Offer not found' });
     }
