c5687aaf384228085785ab86ca0ca7363d405c71
Nikhil mandia <nikhilmandia7@gmail.com>
2024-07-10 15:34:41 +0530

    dsfsdf

--- api/routes/offerRoutes.js
+++ api/routes/offerRoutes.js
@@ -10,14 +10,14 @@ const {
   getSavedOffers,
   grabOffer,
   grabbedOffers,
-  getHomeScreenOffers
+  getHomeScreenOffers,
 } = require("../controllers/offerControllers");
 const upload = require("../middlewares/uploadImage");
 const { userAuth } = require("../middlewares/auth");
 const router = express.Router();
 
-router.post("/offer", upload.single('image'), postOffer);
-router.get("/offer", getOffers);
+router.post("/offer", upload.single("image"), postOffer);
+router.get("/offers", getOffers);
 router.get("/home-offers", getHomeScreenOffers);
 router.get("/offer/:offer_id", getOffer);
 router.put("/offer/:offer_id", putOffer);
@@ -27,7 +27,7 @@ router.get("/saved-offers", userAuth, getSavedOffers);
 router.put("/offer/:offer_id/save", userAuth, saveOffer);
 router.put("/offer/:offer_id/unsave", userAuth, unsaveOffer);
 
-router.put('/offer/:offer_id/grab', userAuth, grabOffer)
-router.get('/grabbed-offers', grabbedOffers)
+router.put("/offer/:offer_id/grab", userAuth, grabOffer);
+router.get("/grabbed-offers", grabbedOffers);
 
 module.exports = router;
